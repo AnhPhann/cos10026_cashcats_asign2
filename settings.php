4f4fd473d0cc321d546e1ed2ada8906248fdf5e4
Anh Phan <104522443@student.swin.edu.au>
2024-05-11 00:22:28 +1000

    require 1-2

---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+	$host = "feenix-mariadb.swin.edu.au";
+	$user = "s104522443"; // your user name
+	$pwd = "cats190205"; // your password (date of birth ddmmyy unless changed)
+	$sql_db = "s104522443_db"; // your database
+?>
